c2ffd1d175ea1f803bf39ba9d290a3338ce06d00
Jacob Vanzella <jacobvanzella@gmail.com>
2023-08-13 21:59:18 -0700

    Change from Vinegar to Oil.

--- lua/plugins/one-line-utils.lua
+++ lua/plugins/one-line-utils.lua
@@ -1,8 +1,8 @@
 return {
     -- Autopairs . . . it . . . it's beatiful. :')
     { 'windwp/nvim-autopairs', event = 'InsertEnter', opts = {} },
-    -- Netrw modernization
-    'tpope/vim-vinegar',
+    -- -- Netrw modernization
+    -- 'tpope/vim-vinegar',
     -- Key hints (nice)
     { 'folke/which-key.nvim',  opts = {} },
     -- Git integration
